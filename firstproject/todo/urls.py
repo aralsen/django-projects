b3ca906b20c7d9198a31da6845b65c7aa26b1cb9
Aral Sen <asen4@binghamton.edu>
2024-05-19 16:27:42 -0400

    feat: CRUD operations

--- firstproject/todo/urls.py
+++ firstproject/todo/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register),
-    path('login/', views.login),
     path('', views.home),
-    path('create-task/', views.create_task)
+    path('register', views.register),
+    path('login', views.login),
+    path('create-task/', views.create_task, name="create-task"),
+    path('view-tasks', views.view_tasks, name="view-tasks"),
+    path('update-task/<str:pk>', views.update_task, name="update-task"),
+    path('delete-task/<str:pk>', views.delete_task, name="delete-task"),
 ]
--- firstproject/todo/views.py
+++ firstproject/todo/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render
-from .models import Task
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 from .forms import TaskForm
+from .models import Task
 
 
 # Create your views here.
 def home(request):
-    query_single = Task.objects.get(id=3)
-
-    context = {
-        "task": query_single
-    }
-
-    return render(request, "index.html", context=context)
+    return render(request, "index.html")
 
 
 def register(request):
@@ -25,8 +21,47 @@ def login(request):
 def create_task(request):
     form = TaskForm()
 
-    context = {
-        "form": form
-    }
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("view-tasks")
 
-    return render(request, "task-form.html", context=context)
+    context = {"form": form}
+
+    return render(request, "create-task.html", context=context)
+
+
+def view_tasks(request):
+    tasks = Task.objects.all()
+
+    context = {"tasks": tasks}
+
+    return render(request, "view-tasks.html", context=context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("view-tasks")
+
+    context = {"form": form}
+
+    return render(request, "update-task.html", context=context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("view-tasks")
+
+    context = {"object": task}
+
+    return render(request, "delete-task.html", context=context)
