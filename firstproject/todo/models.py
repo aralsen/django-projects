5b992a2a4cc0f9e63fbc885a1540b780e717a3b1
Aral Sen <asen4@binghamton.edu>
2024-05-18 23:50:45 -0400

    feat: Intro to Foreign Keys (FK)
    on_delete=models.CASCADE  means that when the referenced object (the foreign key target) is deleted, the objects containing the foreign key will also be deleted.

--- firstproject/todo/models.py
+++ firstproject/todo/models.py
@@ -7,3 +7,9 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=300)
     date_posted = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    reviewer = models.CharField(max_length=200)
+    reviewer_title = models.CharField(max_length=85)
